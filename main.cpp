13279da4f333160545c8862e07e516f10db170fb
Роман Макеев <mykolttime666@gmail.com>
2019-02-26 21:30:06 +0400

    Задание 1.3.2

--- main.cpp
+++ main.cpp
@@ -5,4 +5,21 @@
 //  Created by Роман Макеев on 26/02/2019.
 //
 
-#include "main.hpp"
+#include "std.hpp"
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    
+    int a , b;
+    
+    cin >> a;
+    cin >> b;
+    
+    
+    return 0;
+    
+    
+    
+}
